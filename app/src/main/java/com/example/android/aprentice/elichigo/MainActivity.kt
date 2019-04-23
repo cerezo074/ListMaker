f022f73f1f3a7337c1e56997f01d6f41b139ab2e
Eli Pacheco Hoyos <eli@balancemy.money>
2019-04-22 23:03:54 -0500

    Finish Chapter 8

--- app/src/main/java/com/example/android/aprentice/elichigo/MainActivity.kt
+++ app/src/main/java/com/example/android/aprentice/elichigo/MainActivity.kt
@@ -2,29 +2,57 @@ package com.example.android.aprentice.elichigo
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.text.InputType
+import android.widget.EditText
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var listsRecyclerView: RecyclerView
+    val listDataManager: ListDataManager = ListDataManager(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        val lists = ListDataManager(this)
         listsRecyclerView = findViewById<RecyclerView>(R.id.lists_recycler_view)
         listsRecyclerView.layoutManager = LinearLayoutManager(this)
-        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter()
+        listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter(lists.readLists())
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+           showCreateListDialog()
         }
+    }
+
+    private fun showCreateListDialog() {
+        val dialogTitle = getString(R.string.name_of_list)
+        val positiveButtonTitle = getString(R.string.create_list)
+
+        val builder = AlertDialog.Builder(this)
+        val listTitleEditText = EditText(this)
+        listTitleEditText.inputType = InputType.TYPE_CLASS_TEXT
+
+        builder.setTitle(dialogTitle)
+        builder.setView(listTitleEditText)
+
+        builder.setPositiveButton(positiveButtonTitle, { dialog, i ->
+            val list = TaskList(listTitleEditText.text.toString())
+            listDataManager.saveList(list)
+
+            val recyclerAdapter = listsRecyclerView.adapter as ListSelectionRecyclerViewAdapter
+            recyclerAdapter.addList(list)
+
+            dialog.dismiss()
+        })
+
+        builder.create().show()
     }
 
 }
---
+++ app/src/main/java/com/example/android/aprentice/elichigo/TaskList.kt
@@ -0,0 +1,5 @@
+package com.example.android.aprentice.elichigo
+
+class TaskList(val name: String, val task: ArrayList<String> = ArrayList<String>()) {
+
+}
